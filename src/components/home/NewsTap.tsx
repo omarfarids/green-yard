53ad882b6cb04169f86e65a9760fa544c1714268
omarfarids <omar.farid007@gmail.com>
2023-12-06 10:18:34 +0200

    news tap

---
+++ src/components/home/NewsTap.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+type Props = {};
+
+const NewsTap = (props: Props) => {
+  return (
+    <div className="relative w-full h-12 bg-primary border border-gray overflow-hidden">
+      <div className="absolute news-tap flex flex-row justify-center items-center text-white">
+        <p className="whitespace-nowrap text-lg font-bold">
+          we are aware you're the type of one who needs to implement palmy
+          digital selling campaigns. Thus, for us to meet your standard, you
+          would like the optimum choice of promoting channels, correct metrics.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default NewsTap;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import Intro from "../components/home/Intro";
 import MainPoints from "../components/home/MainPoints";
 import Services from "../components/home/Services";
+import NewsTap from "../components/home/NewsTap";
 
 type Props = {};
 
 const Home = (props: Props) => {
   return (
     <div>
+      <NewsTap />
       <Intro />
       <Services />
       <MainPoints />
